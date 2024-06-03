3527a549e38b16d58827f36bb55985b452ab8dfb
Salvo9879 <salvatorelapaglia@icloud.com>
2024-06-03 23:16:19 +0100

    added routes

--- __init__.py
+++ __init__.py
@@ -2,3 +2,9 @@ from flask import Blueprint
 
 
 application__blueprint = Blueprint('test_app', __name__, url_prefix='/test_app')
+
+
+
+@application__blueprint.route('/')
+def index():
+    return 'success'
--- database.py
+++
@@ -1,9 +0,0 @@
-from main.database import database
-from sqlalchemy.schema import Column
-from sqlalchemy.types import Integer, String
-
-class Test(database.Model):
-    __bind_key__ = 'Test'
-
-    identifier = Column('identifier', Integer(), primary_key=True)
-    name = Column('name', String(16))
